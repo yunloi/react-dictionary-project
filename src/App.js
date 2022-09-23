de546e6a0794c7b288d55ef81e93fa9bd0705c83
yunloi <103883589+yunloi@users.noreply.github.com>
2022-09-23 22:46:16 +1000

    added search engine form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Yun Loi Lok</small>
+        </footer>
+      </div>
     </div>
   );
 }
